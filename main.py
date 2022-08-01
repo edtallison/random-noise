ad71071190218389c2858fdf3ebb9860d93bead7
edtallison <edtallison@gmail.com>
2022-08-01 19:17:55 +1200

    setup imports and plotting

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("hello world")
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import numpy as np
+
+test_image = np.zeros((50,50))
+
+for i in range(len(test_image)):
+    for j in range(len(test_image[i])):
+        test_image[i,j] += (i+j)
+
+imgplot = plt.imshow(test_image)
+
+print(test_image)
+
+plt.show()
