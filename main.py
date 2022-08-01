e165e95d241a3fe2f664c954f51f22d8f837ed75
edtallison <edtallison@gmail.com>
2022-08-01 19:23:46 +1200

    figure out how to get the dang thing in grayscale

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ test_image = np.zeros((50,50))
 
 for i in range(len(test_image)):
     for j in range(len(test_image[i])):
-        test_image[i,j] += (i+j)
+        test_image[i,j] += (i+j)*4
 
-imgplot = plt.imshow(test_image)
+imgplot = plt.imshow(test_image, cmap='gray',vmin=0,vmax=255)
 
 print(test_image)
 
