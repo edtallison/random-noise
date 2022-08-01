45054a2400f05a869a9ecdf72b886af8bf5800e6
edtallison <edtallison@gmail.com>
2022-08-01 20:18:48 +1200

    create random_size function

--- random_funcs.py
+++ random_funcs.py
@@ -11,4 +11,14 @@ def random_point(image):
     return (y, x)
 
 def random_size(image):
-    pass
+    # size measured as radius in pixels
+
+    height = len(image)
+    width = len(image[0])
+
+    max_size = min(height, width) / 2
+    min_size = 5
+
+    size = random.random_integers(min_size, max_size)
+
+    return size
